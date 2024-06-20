40cb0570c2526d7c965152659d07c103cda74c1d
faithadewuyi <iyanuleke09@gmail.com>
2024-06-20 23:21:15 +0100

    Tailwindcss set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,37 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+
+    screens: {
+      xs: "320px",
+      sm: "375px",
+      sms: "386px",
+      smm: "410px",
+      sml: "500px",
+      md: "667px",
+      mdl: "768px",
+      lg: "960px",
+      lgl: "1024px",
+      xl: "1280px",
+    },
+
+    extend: {
+
+      colors: {
+        btn: "#FFD700",
+        footer: "#27272B",
+        textcol: "#000000",
+        black: "#27272B",
+        lightgray: "#CCD4DD",
+        darkGray: "#D9D9D9",
+
+      },
+
+      fontFamily:{
+        mon: [ "Montserrat", "sans-serif;"],
+        inter: ["Inter", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
