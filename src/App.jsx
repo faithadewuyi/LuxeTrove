e31d10e42b86a84be435b38ddad741e73344c996
Esther Gar <bamgboseesther3@gmail.com>
2024-06-26 11:29:02 +0100

    Some of my finished/almost finished work

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Cart } from './pages/Cart';
+import ChatBot from './pages/ChatBot';
+import BecomeSalesRep from './pages/BecomeSalesRep';
+import BecomeLogisticsProvider from './pages/BecomeLogisticsProvider';
+import BecomeVendorHub from './pages/BecomeVendorHub';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      {/*
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <BecomeVendorHub />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+       */}
+
+      {/* 
+      <div>
+        <BecomeLogisticsProvider />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      */}
+      {/*
+      <div>
+        <BecomeSalesRep />
+      </div>
+       */}
+    
+
+      
+      <div>
+        <ChatBot />
+      </div>
+      
+      {/*
+      <div>
+        <Cart />
+      </div>
+      */}
     </>
   )
-}
-
+};
 export default App
